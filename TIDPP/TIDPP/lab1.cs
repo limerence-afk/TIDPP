b3dd625f5854e495dd2e9a27befb65084a7abc7e
Bogdan <maxim.bogdan98@gmail.com>
2020-09-08 17:48:14 +0300

    добавил новый файл

---
+++ TIDPP/TIDPP/lab1.cs
@@ -0,0 +1,11 @@
+{
+     class Program
+{
+     static void Main(string[] args)
+     {
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.WriteLine("Hello World");
+          Console.ReadLine();
+     }
+}
+}
